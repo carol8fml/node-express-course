d847aa3c5122b8cd42ffd6c94b68301e7324be5c
carol8fml <anacaroliinafjv@gmail.com>
2021-09-27 11:44:30 -0300

    add login POST route

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
 
+app.get("/users", function (req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData,
+  });
+});
+// dois pontos são usados como variáveis que podem ser vistas nos parâmetros
 app.get("/users/:id", function (req, res) {
   console.log(req.params.id);
   res.json({
@@ -12,12 +23,28 @@ app.get("/users/:id", function (req, res) {
   });
 });
 
-app.get("/users", function (req, res) {
-  res.json({
-    success: true,
-    message: "successfully got users. Nice!",
-    users: mockUserData,
-  });
+app.post("/login", function (req, res) {
+  // Normalmente, as senhas são criptografadas usando algo como bcrypt antes de enviar para o banco de dados
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Isso deve vir do banco de dados
+  const mockUsername = "billyTheKid";
+  const mockPassword = "superSecret";
+
+  if (username === mockUsername && password === mockPassword) {
+    // Na prática, use o método de assinatura de token da web JSON aqui para fazer um token criptografado
+    res.json({
+      success: true,
+      message: "password and username match!",
+      token: "encrypted token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "password and username do not match",
+    });
+  }
 });
 
 app.listen(8000, function () {
